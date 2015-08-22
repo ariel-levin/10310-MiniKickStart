2840a23e5c3cb0afa57920b413d44b1622951d87
Ariel Levin <ariel.lvn89@gmail.com>
2015-08-22 22:56:10 +0300

    login and register modals

--- src/js/app.js
+++ src/js/app.js
@@ -22,14 +22,39 @@ kickstartApp.controller('AppCtrl', ['$scope', 'User',
 
         $scope.signedIn = false;
 
-        $scope.login = function () {
-            $scope.signedIn = true;
-            alert("logged in");
+        $scope.login = {};
+        $scope.register = {};
+
+        $scope.login = function (valid) {
+            if (valid) {
+                $scope.signedIn = true;
+
+                alert("login success\n"
+                    + "\nemail: " + $scope.login.email
+                    + "\npass: " + $scope.login.pass);
+
+                $('#loginModal').modal('toggle');
+            } else {
+                alert("some login form fields are incorrect");
+            }
         };
 
         $scope.logout = function () {
             $scope.signedIn = false;
-            alert("logged out");
+            alert("logged out success");
+        };
+
+        $scope.register = function (valid) {
+            if (valid) {
+                alert("register success\n"
+                    + "\nname: " + $scope.register.firstName + " " + $scope.register.lastName
+                    + "\nemail: " + $scope.register.email
+                    + "\npass: " + $scope.register.pass);
+
+                $('#registerModal').modal('toggle');
+            } else {
+                alert("some register form fields are incorrect");
+            }
         };
 
     }
